6175ba615c9ef2faaddab54117fa8d2dc7fa3837
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2023-01-13 16:31:11 -0300

    Estrutura da Página de Produto

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
   
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        
+        <p>Lorem ipsum dolor sit amet. Et autem quibusdam aut veniam consequuntur sit perferendis dolores et ipsam voluptatem quo velit vitae. Ut Quis impedit in deserunt reiciendis nam excepturi officiis qui eveniet fugit nam quibusdam dolorum quo veniam dolores et consequatur adipisci.</p>
+        
+        <button>
+          Comprar Agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
