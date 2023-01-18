095122a4ac70a80cd30d0c29f578fb7ec57974c7
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2023-01-18 10:38:13 -0300

    Checkout no Stripe

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -13,10 +13,15 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
+  
   const { isFallback } = useRouter()
   
   if (isFallback) {
@@ -33,7 +38,7 @@ export default function Product({ product }: ProductProps) {
         <span>{product.price}</span>
         <p>{product.description}</p>
         
-        <button>
+        <button onClick={handleBuyProduct}>
           Comprar Agora
         </button>
       </ProductDetails>
@@ -72,6 +77,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
             currency: 'BRL'
           }).format(price.unit_amount! / 100),
           description: product.description,
+          defaultPriceId: price.id,
         }
     },
     revalidate: 60 * 60 * 1, // 1 hour
